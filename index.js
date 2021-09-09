386486856be8661efcc6af11129afa8d4da7d90e
Jonathan Talavera <jtalavera@g.ucla.edu>
2021-09-09 16:49:15 -0700

    Etch a sketch setup

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hit");
+const DEFAULT_SIZE = 3;
+
+const grid = document.getElementById('grid');
+
+function setupGrid (size) {
+
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+    for (let i = 0 ; i < size * size; i++) {
+        const gridElement = document.createElement('div');
+        grid.appendChild(gridElement);
+    }
+}
+
+
+window.onload = () => {
+    setupGrid(DEFAULT_SIZE);
+}
