3e88b90285f23bf75b5bfade03be08a1853c792a
Jonathan Talavera <jtalavera@g.ucla.edu>
2021-09-09 17:13:10 -0700

    Incremental improvement is difficult

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const grid = document.getElementById('grid');
 
 function setupGrid (size) {
 
-    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
-
+    // grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    // grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
     for (let i = 0 ; i < size * size; i++) {
         const gridElement = document.createElement('div');
         grid.appendChild(gridElement);
