43a1b5eb68076212eef85982cfaf6b5bbffa6d89
Jonathan Talavera <jtalavera@g.ucla.edu>
2021-09-27 17:44:15 -0700

    Meaningful commit

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const DEFAULT_SIZE = 3;
+const DEFAULT_SIZE = 2;
 
 const grid = document.getElementById('grid');
 
 function setupGrid (size) {
 
-    // grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-    // grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
     for (let i = 0 ; i < size * size; i++) {
         const gridElement = document.createElement('div');
         grid.appendChild(gridElement);
     }
+
+}
+
+function changeColor () {
+    
 }
 
 
